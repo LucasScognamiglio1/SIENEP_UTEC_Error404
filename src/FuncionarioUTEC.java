f351939b071c36233b60a6921ed753a1defcbf64
Cecilia Dean <cecilia.dean@estudiantes.utec.edu.uy>
2025-06-26 20:28:14 -0300

    Se agrega clase FuncionarioUTEC que hereda de Usuario

---
+++ src/FuncionarioUTEC.java
@@ -0,0 +1,7 @@
+public class FuncionarioUTEC extends Usuario {
+
+    public FuncionarioUTEC(int id, String nombre, String email, String password, Rol rol) {
+            super(id, nombre, email, password, rol);
+    }
+}
+

474c612a83088bedd455e2b5eed2aec255f6baea
Cecilia Dean <cecilia.dean@estudiantes.utec.edu.uy>
2025-06-26 20:27:33 -0300

    Se agrega clase AnalistaEducativo que hereda de Usuario

---
+++ src/AnalistaEducativo.java
@@ -0,0 +1,6 @@
+public class AnalistaEducativo extends Usuario {
+
+    public AnalistaEducativo(int id, String nombre, String email, String password, Rol rol) {
+        super(id, nombre, email, password, rol);
+    }
+}
